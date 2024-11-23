ba4094eefe54dc00ebf080b30237c59de129239d
ParsaJafarian <003paria.j@gmail.com>
2024-11-23 15:47:45 -0500

    add resize feature

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,48 +1,44 @@
-import { Suspense, useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
 import Loader from "../components/loader";
 import Piano from "@/models/Piano";
 import { Navbar } from "@/components/Nabvar";
 import HomeText from "@/components/HomeText";
-import MusicNotes from "@/components/MusicNotes";
 
 const Home = () => {
+  const [pianoScale, setPianoScale] = useState([0, 0, 0]);
+  const [pianoPosition, setPianoPosition] = useState([0, 0, 0]);
+  const [pianoRotation, setPianoRotation] = useState([0, 0, 0]);
+
   const adjustModelForScreenSize = () => {
-    let screenScale = null;
-    const screenPosition = [35, 1.5, -43];
-    const rotation = [0, Math.PI, 5];
-
-    if (window.innerWidth < 480) {
-      screenScale = [1.0, 1.0, 1.0];
-      screenPosition[0] = 30;
-    } else if (window.innerWidth < 768) {
-      screenScale = [1.2, 1.2, 1.2];
-      screenPosition[0] = 32;
-    } else if (window.innerWidth < 1024) {
-      screenScale = [2.0, 2.0, 2.0];
-      screenPosition[0] = 34;
+    if (window.innerWidth < 768) {
+      setPianoScale([1.0, 1.0, 1.0]);
+      setPianoPosition([30, 1.5, -43]);
+    } else if (window.innerWidth < 1370) {
+      setPianoScale([1.8, 1.8, 1.8]);
+      setPianoPosition([25, 1.5, -43]);
     } else {
-      screenScale = [2.5, 2.5, 2.5];
-      screenPosition[0] = 35;
+      setPianoScale([2.5, 2.5, 2.5]);
+      setPianoPosition([35, 1.5, -43]);
     }
-
-    return { screenScale, screenPosition, rotation };
   };
 
-  const {
-    screenScale: pianoScale,
-    screenPosition: pianoPosition,
-    rotation: pianoRotation,
-  } = adjustModelForScreenSize();
+  useEffect(() => {
+    adjustModelForScreenSize();
+    window.addEventListener("resize", () => adjustModelForScreenSize());
+
+    return () =>
+      window.removeEventListener("resize", () => adjustModelForScreenSize());
+  }, []);
 
   return (
-    <div className="relative min-h-screen">
+    <div className="w-full h-screen relative">
       <HomeText />
       <section className="w-full h-screen relative bg-red-500">
         <Navbar />
 
         <Canvas
-          className="w-full h-screen bg-transparent absolute top-0 left-0"
+          className="w-full min-h-screen bg-transparent absolute top-0 left-0"
           camera={{ near: 0.1, far: 1000 }}
         >
           <Suspense fallback={<Loader />}>
