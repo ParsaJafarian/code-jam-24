231ec09ae7212192bd0248fc987d26503b076889
Paria Jafarian <003paria.j@gmail.com>
2024-11-23 09:31:40 -0500

    add rotation and drag to piano

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,19 +1,19 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import Loader from "../components/loader";
 import Piano from "@/models/Piano";
 
 const Home = () => {
   const adjustModelForScreenSize = () => {
     let screenScale = null;
-    const screenPosition = [0, -6.5, 0.9];
+    const screenPosition = [0, -6.5, -43];
     const rotation = [0.1, 4.7, 0];
 
     if (window.innerWidth < 768) {
-      screenScale = [0.6, 0.6, 0.6];
+      screenScale = [0.9, 0.9, 0.9];
     } else {
-      screenScale = [0.7, 0.7, 0.7];
-    }
+      screenScale = [1.6, 1.6, 1.6];
+    } 
 
     return { screenScale, screenPosition, rotation };
   };
@@ -31,7 +31,13 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <ambientLight intensity={0.5} />
+          <directionalLight intensity={3} position={[1, 1, 1]} />
+          <ambientLight intensity={1} />
+          <hemisphereLight 
+            color="#b1e1ff" 
+            groundColor="#000000" 
+            intensity={1} 
+          />
           <Piano
             scale={pianoScale}
             position={pianoPosition}
