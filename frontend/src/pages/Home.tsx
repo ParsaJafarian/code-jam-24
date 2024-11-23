9992f230865befd8a9223898be60a59cb57f4b29
ParsaJafarian <003paria.j@gmail.com>
2024-11-23 14:35:30 -0500

    removed h-screen

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,14 +13,14 @@ const Home = () => {
     const rotation = [0, Math.PI, 5];
 
     if (window.innerWidth < 480) {
-      screenScale = [1.0, 1.0, 1.0];
-      screenPosition[0] = 30;
+      screenScale = [0.8, 0.8, 0.8];
+      screenPosition[0] = 28;
     } else if (window.innerWidth < 768) {
       screenScale = [1.2, 1.2, 1.2];
-      screenPosition[0] = 32;
+      screenPosition[0] = 30;
     } else if (window.innerWidth < 1024) {
-      screenScale = [2.0, 2.0, 2.0];
-      screenPosition[0] = 34;
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition[0] = 0;
     } else {
       screenScale = [2.5, 2.5, 2.5];
       screenPosition[0] = 35;
@@ -38,9 +38,9 @@ const Home = () => {
   return (
     <div className="relative min-h-screen">
       <HomeText />
-      <section className="w-full h-screen relative bg-red-500">
+      <section className="w-full relative bg-red-500">
         <Navbar />
-
+        <HomeText />
         <Canvas
           className="w-full h-screen bg-transparent absolute top-0 left-0"
           camera={{ near: 0.1, far: 1000 }}
