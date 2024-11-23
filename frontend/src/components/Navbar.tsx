ee57c0a7da7ed6db934b33c232699a851f520224
ParsaJafarian <003paria.j@gmail.com>
2024-11-23 18:59:01 -0500

    added logo to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,14 @@
+import logo from '../assets/images/logo4.png';
 
 export function Navbar() {
     return (
-        <header className="px-4 lg:px-8 h-14 flex bg-red-800 sticky top-0 z-50">
+        <header className="px-4 lg:px-8 h-14 flex bg-[#bd1414] sticky top-0 z-50">
             <a className="flex items-center" href="/">
-                <span className="font-bold text-xl text-white">Museify</span>
+                <img 
+                    src={logo} 
+                    alt="Museify Logo" 
+                    className="h-24 sm:h-32"
+                />
             </a>
             <div className="flex items-center justify-end w-full">
                 <nav className="hidden md:flex gap-8">
