c48f0ebe2d5c9a29643d6d34a587a6b6cd6d6170
ParsaJafarian <003paria.j@gmail.com>
2024-11-23 20:36:45 -0500

    replaced a tags with Link tags

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,20 +1,21 @@
+import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo4.png';
 
 export function Navbar() {
     return (
-        <header className="px-4 lg:px-8 h-14 flex bg-[#bd1414] sticky top-0 z-50">
-            <a className="flex items-center" href="/">
+        <header className="px-4 lg:px-8 h-14 flex bg-[#990909] sticky top-0 z-50">
+            <Link className="flex items-center" to="/">
                 <img 
                     src={logo} 
                     alt="Museify Logo" 
                     className="h-24 sm:h-32"
                 />
-            </a>
+            </Link>
             <div className="flex items-center justify-end w-full">
                 <nav className="hidden md:flex gap-8">
-                    <a className="text-sm font-medium text-white hover:text-red-200 hover:underline underline-offset-4" href="/reader">
+                    <Link className="text-sm font-medium text-white hover:text-red-200 hover:underline underline-offset-4" to="/reader">
                         Get Started
-                    </a>
+                    </Link>
                 </nav>
             </div>
         </header>
